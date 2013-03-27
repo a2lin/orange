682aadfa3550feb01ed715ccd3c7d7a933646940
Alexander Lin <lin.alexander.2011@gmail.com>
2013-03-27 16:25:10 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Orange::Application.config.secret_token = '84be3b587a620837adf167e155685ec6b9b36b8b0eb50d170f7214b9d875efee2122e487d1832a93b841bd4eb671030bc36e13eb71a4c44b4c498f7aa309c6cd'
